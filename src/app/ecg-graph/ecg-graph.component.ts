694fd56de79284e0bd4437270f9664d531d423d0
Getulio Souza <getuliosouza8@gmail.com>
2024-12-02 16:30:58 -0300

    bugfix: time-in-seconds-and-minutes-on-screen

--- src/app/ecg-graph/ecg-graph.component.ts
+++ src/app/ecg-graph/ecg-graph.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { timeEnd } from 'console';
 
 @Component({
   selector: 'app-ecg-graph',
@@ -7,86 +6,122 @@ import { timeEnd } from 'console';
   styleUrls: ['./ecg-graph.component.scss']
 })
 export class EcgGraphComponent implements OnInit {
-  
-  //variaveis 
+
   private time: number[] = [];
   private ecgSignal: number[] = [];
   private heartRateInterval: any;
-  private sampleRate =40;
+  private sampleRate = 40;
   private bpm: number = 60;
   private duration: number = 10;
   private maxLength: number = 2500;
   private plotlyInstance: any;
   private currentTime: number = 0;
-  private timeElapsed: number =0;
+  private timeElapsed: number = 0;
 
   ngOnInit(): void {
-    if(typeof window !== 'undefined'){
+    if (typeof window !== 'undefined') {
       this.createECGChart();
     }
   }
 
-  //cria o grafico animado de batimentos cardiacos
-  createECGChart(){
+  createECGChart() {
     import('plotly.js-dist').then(Plotly => {
       this.plotlyInstance = Plotly;
 
-      this.heartRateInterval = setInterval(()=> {
+      // Inicializando o gráfico
+      const trace = {
+        x: this.time,
+        y: this.ecgSignal,
+        type: 'scatter',
+        mode: 'lines',
+        name: 'Heart Rate Signal',
+        line: {
+          color: 'green',
+          width: 2,
+          shape: 'spline',
+          smoothing: 2.0
+        }
+      };
+
+      const layout = {
+        title: 'Sinal ECG em Tempo Real',
+        xaxis: {
+          title: 'Tempo Transcorrido',
+          showgrid: true,
+          zeroline: true,
+          ticks: 'outside',
+          tickmode: 'array',
+          tickvals: this.getXaxisLayout().tickvals,
+          ticktext: this.getXaxisLayout().ticktext
+        },
+        yaxis: {
+          title: 'Número de Batimentos (BPM)',
+          range: [0, 200],
+          showgrid: true,
+          zeroline: true,
+          ticks: 'outside'
+        },
+        showlegend: false,
+        plot_bgcolor: 'white',
+      };
+
+      this.plotlyInstance.newPlot('ecg-plot', [trace], layout);  // Plotando o gráfico inicialmente
+
+      // Atualizando o gráfico a cada 100ms
+      this.heartRateInterval = setInterval(() => {
         this.updateECGData();
         this.updateGraph();
-        this.currentTime +=1;
-        this.timeElapsed +=1;
+        this.currentTime += 0.1;  // Atualiza o tempo em incrementos de 100ms
+        this.timeElapsed += 0.1;
 
-        if(this.timeElapsed >= 10){
+        if (this.timeElapsed >= 10) {
           this.resetBPM();
-          this.timeElapsed = 0
+          this.timeElapsed = 0;
         }
-      },9000 / this.sampleRate)
-    }).catch((error)=> {
-      console.log('error loading Plotly ECG', error)
-    })
+
+      }, 100); // Atualiza o gráfico 10 vezes por segundo
+    }).catch((error) => {
+      console.log('Erro ao carregar Plotly ECG', error);
+    });
   }
 
-  resetBPM(){
-    this.bpm = Math.floor(Math.random() * (80 - 60 + 1)) + 30;
-    console.log('BPM reset to:' , this.bpm)
+  resetBPM() {
+    this.bpm = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
+    console.log('BPM resetado para:', this.bpm);
   }
 
-  updateECGData(){
-    // const timePoint = this.time.length > 0 ? this.time[this.time.length -1] + 1 /this.sampleRate : 0;
+  updateECGData() {
     const timePoint = this.currentTime;
-    console.log('timePoint:', timePoint)
-    const heartBeat = this.generateHeathBeat(timePoint);
-    console.log('heartBeat', heartBeat)
-    this.time.push(timePoint)
-    this.ecgSignal.push(heartBeat)
+    console.log('timePoint:', timePoint);
 
-    //mantendo o comprimento do sinal ECG no limite da largura da tela
-    if(this.time.length > this.maxLength){
+    const heartBeat = this.generateHeathBeat(timePoint) *60;
+    console.log('heartBeat', heartBeat);
+
+    this.time.push(timePoint);
+    this.ecgSignal.push(heartBeat);
+
+    if (this.time.length > this.maxLength) {
       this.time.shift();
       this.ecgSignal.shift();
     }
   }
 
-  //method to generate the heath beat from time to time
-  generateHeathBeat(time: number): number{
-    // const heartBeatCycleDuration = 100 / this.bpm;
-    const heartBeatCycleDuration = 10;
+  generateHeathBeat(time: number): number {
+    const heartBeatCycleDuration = 60 / this.bpm;  // Ciclo de batimento com base no BPM
     const relativeTime = time % heartBeatCycleDuration;
 
-    if(relativeTime < 7){
+    if (relativeTime < 7) {
       return 1.0 + Math.random() * 0.1;
     }
 
-    if(relativeTime >= 7 && relativeTime < 9){
+    if (relativeTime >= 7 && relativeTime < 9) {
       return 3.0 * Math.random() * 0.5;
     }
 
     return 1.0 + Math.random() * 0.1;
   }
 
-//atualiando o graficoECG de tempos em tempos
-  updateGraph(){
+  updateGraph() {
     const trace = {
       x: this.time,
       y: this.ecgSignal,
@@ -97,39 +132,35 @@ export class EcgGraphComponent implements OnInit {
         color: 'green',
         width: 2,
         shape: 'spline',
-        smoothing:2.0
+        smoothing: 2.0
       }
-    }
+    };
 
     const layout = {
-      title: 'Real-Time ECG Signal',
-
+      title: 'Sinal ECG em Tempo Real',
       xaxis: {
-        title: 'Time (s)',
+        title: 'Tempo Transcorrido',
         showgrid: true,
-        zeroline: true
+        zeroline: true,
+        ticks: 'outside',
+        tickmode: 'array',
+        tickvals: this.getXaxisLayout().tickvals,
+        ticktext: this.getXaxisLayout().ticktext
       },
-
       yaxis: {
-        title: 'Amplitude (mV)',
+        title: 'Número de Batimentos (BPM)',
+        range: [50, 100],
         showgrid: true,
-        zeroline: true
+        zeroline: true,
+        ticks: 'outside'
       },
-
       showlegend: false,
       plot_bgcolor: 'white',
-      // updatemenus: [
+    };
 
-      // ]
-    }
-
-    const data = [trace];
-
-    if(this.time.length <= this.maxLength){
-      if(this.plotlyInstance){
-        this.plotlyInstance.newPlot('ecg-plot', data, layout);
-      }
-    } else{
+    if (this.time.length <= this.maxLength) {
+      this.plotlyInstance.newPlot('ecg-plot', [trace], layout);
+    } else {
       this.plotlyInstance.update('ecg-plot', {
         x: [this.time],
         y: [this.ecgSignal]
@@ -137,4 +168,31 @@ export class EcgGraphComponent implements OnInit {
     }
   }
 
+  getXaxisLayout() {
+    const tickvals: number[] = [];
+    const ticktext: string[] = [];
+    let timeInSeconds = Math.floor(this.currentTime);
+
+    console.log('time:', timeInSeconds);
+
+    // Convertendo 60 segundos em milissegundos
+    if (timeInSeconds < 60) {
+      for (let i = 0; i <= timeInSeconds; i++) {
+        tickvals.push(i);
+        ticktext.push(`${i}s`);
+      }
+    } else if (timeInSeconds >= 60 && timeInSeconds < 3600) {
+      for (let i = 0; i < Math.floor(timeInSeconds / 60); i++) {
+        tickvals.push(i * 60);
+        ticktext.push(`${i + 1}m`);
+      }
+    } else {
+      for (let i = 0; i < Math.floor(timeInSeconds / 3600); i++) {
+        tickvals.push(i * 3600);
+        ticktext.push(`${i + 1}h`);
+      }
+    }
+
+    return { tickvals, ticktext };
+  }
 }
