64d3003d0edd039ffba0017b46474306225e6618
Getulio Souza <getuliosouza8@gmail.com>
2024-12-02 08:56:20 -0300

    feature: install-plotly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EcgGraphComponent } from './ecg-graph/ecg-graph.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: EcgGraphComponent}
+];
---
+++ src/app/ecg-graph/ecg-graph.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ecg-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './ecg-graph.component.html',
+  styleUrl: './ecg-graph.component.scss'
+})
+export class EcgGraphComponent {
+
+}
